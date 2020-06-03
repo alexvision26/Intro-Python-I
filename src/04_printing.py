e52b12c10f724897c2978baa06716a019fc1d6c4
Alex <alexmillerprojects@gmail.com>
2020-06-02 19:19:20 -0700

    completed project

--- src/04_printing.py
+++ src/04_printing.py
@@ -12,7 +12,7 @@ z = "I like turtles!"
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
 
-# print("x is %s, y is %.2f, z is '%s'" % (x, y, z,))
+print("x is %s, y is %.2f, z is '%s'" % (x, y, z,))
 
 # Use the 'format' string method to print the same thing
 
@@ -21,4 +21,4 @@ print(myStr.format(x, y, z))
 
 # Finally, print the same thing using an f-string
 
-# print(f"x is {x}, y is {y}, z is '{z}''")
+print(f"x is {x}, y is {y}, z is '{z}''")
--- src/11_args.py
+++ src/11_args.py
@@ -6,7 +6,10 @@
 
 # YOUR CODE HERE
 
-print(f1(1, 2))
+def f1(a, b):
+    return a + b
+
+# print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and returns the
 # sum.
@@ -14,46 +17,65 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
-print(f2(1))                    # Should print 1
-print(f2(1, 3))                 # Should print 4
-print(f2(1, 4, -12))            # Should print -7
-print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
+def f2(*nums):
+    new_val = []
+    for x in nums:
+        new_val.append(x)
+    if type(nums[0]) == list:
+        return sum(nums[0])
+    elif type(nums[0]) == int:
+        return sum(nums)
 
-a = [7, 6, 5, 4]
+# print(f2(1))                    # Should print 1
+# print(f2(1, 3))                 # Should print 4
+# print(f2(1, 4, -12))            # Should print -7
+# print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 
-# How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+# a = [7, 6, 5, 4]
+# print(f2(a))    # Should print 22
 
-# Write a function f3 that accepts either one or two arguments. If one argument,
-# it returns that value plus 1. If two arguments, it returns the sum of the
-# arguments.
-# Note: Google "python default arguments" for a hint.
+# # Write a function f3 that accepts either one or two arguments. If one argument,
+# # it returns that value plus 1. If two arguments, it returns the sum of the
+# # arguments.
+# # Note: Google "python default arguments" for a hint.
 
-# YOUR CODE HERE
+# # YOUR CODE HERE
 
-print(f3(1, 2))  # Should print 3
-print(f3(8))     # Should print 9
+def f3(*num):
+    new_val = [x for x in num]
+    if len(new_val) == 1:
+        return sum(new_val) + 1
+    elif len(new_val) > 1:
+        return sum(new_val)
+
+# print(f3(1, 2))  # Should print 3
+# print(f3(8))     # Should print 9
 
 
-# Write a function f4 that accepts an arbitrary number of keyword arguments and
-# prints out the keys and values like so:
-#
-# key: foo, value: bar
-# key: baz, value: 12
-#
-# Note: Google "python keyword arguments".
+# # Write a function f4 that accepts an arbitrary number of keyword arguments and
+# # prints out the keys and values like so:
+# #
+# # key: foo, value: bar
+# # key: baz, value: 12
+# #
+# # Note: Google "python keyword arguments".
 
-# YOUR CODE HERE
+# # YOUR CODE HERE
+
+def f4(**words):
+    for key, value in words.items():
+        print("key: " + str(key) + ", value: " + str(value))
+
 
 # Should print
 # key: a, value: 12
 # key: b, value: 30
 f4(a=12, b=30)
 
-# Should print
-# key: city, value: Berkeley
-# key: population, value: 121240
-# key: founded, value: "March 23, 1868"
+# # Should print
+# # key: city, value: Berkeley
+# # key: population, value: 121240
+# # key: founded, value: "March 23, 1868"
 f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
 d = {
@@ -61,5 +83,5 @@ d = {
     "hp": 3
 }
 
-# How do you have to modify the f4 call below to make this work?
-f4(d)
+# # How do you have to modify the f4 call below to make this work?
+f4(**d)
